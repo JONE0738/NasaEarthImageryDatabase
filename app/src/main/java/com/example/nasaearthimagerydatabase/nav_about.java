1bc906619c4b60ffbd3ef5f8e81a9ec56c30c7a4
Andrew <bart0263@algonquinlive.com>
2021-12-17 17:56:11 -0500

    update

--- app/src/main/java/com/example/nasaearthimagerydatabase/nav_about.java
+++ app/src/main/java/com/example/nasaearthimagerydatabase/nav_about.java
@@ -4,20 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class nav_about extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,25 @@ public class nav_about extends AppCompatActivity {
                 Toast.makeText(nav_about.this, "Andrew Barton: 041026297", Toast.LENGTH_LONG ).show();
             }
         });
+
+        Button btnbartonimage = (Button) findViewById(R.id.bartonimage);
+        btnbartonimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar1 = Snackbar.make(btnbartonimage, "Andrew Barton, resides in Burlington, Ontario", Snackbar.LENGTH_LONG);
+                        snackbar1.show();
+            }
+        });
+
+        Button btnjonesimage = (Button) findViewById(R.id.jonesimage);
+        btnjonesimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar1 = Snackbar.make(btnjonesimage, "Andrew Jones, resides in St. Catharines, Ontario", Snackbar.LENGTH_LONG);
+                snackbar1.show();
+            }
+        });
+
 
         drawerLayout = findViewById(R.id.my_drawer_layout_about);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
@@ -77,7 +101,7 @@ public class nav_about extends AppCompatActivity {
         // setup the alert builder
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Help menu:");
-        builder.setMessage("Welcome to the About page. If you would like to learn more about the authors, please click the button below their name.");
+        builder.setMessage("Welcome to the About page. If you would like to learn more about the authors, please click the button below their name. For additional information, feel free to click on their pictures.");
         // add a button
         builder.setPositiveButton("OK", null);
         // create and show the alert dialog
