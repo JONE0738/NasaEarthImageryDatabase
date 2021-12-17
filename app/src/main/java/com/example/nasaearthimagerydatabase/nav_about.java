cf67dbfbbcdec9b25cd127199e1fff6075f4472d
Andrew Jones <ajones0030@gmail.com>
2021-12-16 21:37:29 -0500

    added nav bar and help alert to each page

--- app/src/main/java/com/example/nasaearthimagerydatabase/nav_about.java
+++ app/src/main/java/com/example/nasaearthimagerydatabase/nav_about.java
@@ -1,14 +1,65 @@
 package com.example.nasaearthimagerydatabase;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
 
 public class nav_about extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_about);
+
+        drawerLayout = findViewById(R.id.my_drawer_layout_about);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        //listener to toggle button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //make nav icon appear on action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_about:
+
+                Intent intent = new Intent(this, nav_about.class);
+                startActivity(intent);
+                return true;
+        }
+            //make items appear in nav drawer
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void aboutHelpDialog(View view) {
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Help menu:");
+        builder.setMessage("Welcome to the About page. If you would like to learn more about the authors, please click the button below their name.");
+        // add a button
+        builder.setPositiveButton("OK", null);
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/nasaearthimagerydatabase/nav_favourites.java
+++ app/src/main/java/com/example/nasaearthimagerydatabase/nav_favourites.java
@@ -1,15 +1,62 @@
 package com.example.nasaearthimagerydatabase;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class nav_favourites extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_favourites);
+
+        drawerLayout = findViewById(R.id.my_drawer_layout_favourites);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        //listener to toggle button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //make nav icon appear on action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_favourites:
+
+                Intent intent = new Intent(this, nav_favourites.class);
+                startActivity(intent);
+                return true;
+        }
+        //make items appear in nav drawer
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void favouritesHelpDialog(View view) {
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Help menu:");
+        builder.setMessage("Welcome to the Favourites page. When you save an image as a favourite, it will show up here. Click the image to see additional details.");
+        // add a button
+        builder.setPositiveButton("OK", null);
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
