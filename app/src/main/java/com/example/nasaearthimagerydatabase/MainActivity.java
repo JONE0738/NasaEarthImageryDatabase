d8976cffe278a64aac0865815df58c7f7dffcf76
Andrew Jones <ajones0030@gmail.com>
2021-12-18 19:23:11 -0500

    made the buttons work lol

--- app/src/main/java/com/example/nasaearthimagerydatabase/MainActivity.java
+++ app/src/main/java/com/example/nasaearthimagerydatabase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -39,14 +41,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         actionBarDrawerToggle.syncState();
         //make nav icon appear on action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
         //initialize navigation view for handling nav menu onclicks
         navigationView = (NavigationView) findViewById(R.id.NavView);
         navigationView.setNavigationItemSelectedListener(this);
+        //code for toolbar buttons onclick
+        ImageButton buttonCoordinates = (ImageButton) findViewById(R.id.toolbarCoordinates);
+        buttonCoordinates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCoordinates = new Intent(MainActivity.this, nav_coordinates.class);
+                startActivity(intentCoordinates);
+            }
+        });
+        //code for toolbar buttons onclick
+        ImageButton buttonFavourites = (ImageButton) findViewById(R.id.toolbarFavourites);
+        buttonFavourites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentFavourites = new Intent(MainActivity.this, nav_favourites.class);
+                startActivity(intentFavourites);
+            }
+        });
+        //code for toolbar buttons onclick
+        ImageButton buttonAbout = (ImageButton) findViewById(R.id.toolbarAbout);
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentAbout = new Intent(MainActivity.this, nav_about.class);
+                startActivity(intentAbout);
+            }
+        });
     }
 
+
+        //handle selection of nav drawer menu items
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -71,28 +99,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    //menu toggle on press
+    //appbar menu items
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        //drawer toggle return
         actionBarDrawerToggle.onOptionsItemSelected(item);
         return true;
     }
-
+        //alertDialog method
         public void mainHelpDialog (View view){
-            // setup the alert builder
+            //setup the alert builder
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Help menu:");
             builder.setMessage("Welcome to the Nasa Earth Imagery Database! To search for an image, go to Find Images. To check your favourites, go to the Favourites page. IF you'd like to learn more about the authors, please click on the About page!");
-            // add a button
+            //add a button
             builder.setPositiveButton("OK", null);
-            // create and show the alert dialog
+            //create and show the alert dialog
             AlertDialog dialog = builder.create();
             dialog.show();
         }
 
-
-
 }
+
+
+
 
 
